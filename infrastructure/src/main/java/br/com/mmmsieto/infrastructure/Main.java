8723440a016a0f3a1f9e0cfea1f2022262e6ebdc
devmarciosieto <mmmsieto@hotmail.com>
2023-12-25 11:59:43 -0300

    starting configuration spring-boot

--- infrastructure/src/main/java/br/com/mmmsieto/infrastructure/Main.java
+++ infrastructure/src/main/java/br/com/mmmsieto/infrastructure/Main.java
@@ -1,12 +1,24 @@
 package br.com.mmmsieto.infrastructure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import br.com.mmmsieto.application.UseCase;
+import br.com.mmmsieto.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+
+
+@SpringBootApplication
 public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-       // System.out.println(new UseCase().execute());
+        LOG.info("[step:to-be-init] [id:1] Inicializando o Spring");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
+        LOG.info("[step:inittialized] [id:2] Spring inicializado..");
     }
 
 }
---
+++ infrastructure/src/main/java/br/com/mmmsieto/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.com.mmmsieto.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "br.com.mmmsieto")
+public class WebServerConfig {
+}
